774d46e6d0b6c928cac5448fd3763c6b1c090c1d
Laureen EKON <ekonlaureen16@gmail.com>
2025-10-06 17:12:28 +0000

    changement de l'image des pokemon

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ btn.addEventListener('click', () => {
         .then(res => res.json())
         .then(data => {
             document.getElementById('pokemon-name').innerText = data.name;
-            document.getElementById('pokemon-img').src = data.sprites.front_default;
+            document.getElementById('pokemon-img').src = data.sprites.other['official-artwork'].front_default;
             document.getElementById('pokemon-type').innerText = data.types.map(t => t.type.name).join(', ');
         })
         .catch(() => alert("Pokemon non trouve !"));
