4fa9864727ffae8819243de393bce806e7d094b1
Laureen EKON <ekonlaureen16@gmail.com>
2025-10-07 18:17:26 +0000

    style

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ btn.addEventListener('click', () => {
             document.getElementById('pokemon-img').src = data.sprites.front_default;
             document.getElementById('pokemon-type').innerText = data.types.map(t => t.type.name).join(', ');
             document.getElementById('pokemon-gif').src = data.sprites.other['showdown'].front_default;
-        })
-        .catch(() => alert("Pokemon non trouve !"));
+        
+            // Deuxième requête pour la description :
+            return fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+    })
+    .then(res => res.json())
+    .then(speciesData => {
+      const flavor = speciesData.flavor_text_entries.find(e => e.language.name === "en");
+      document.getElementById('pokemon-description').innerText = flavor ? flavor.flavor_text : "No description available.";
+    })
+    .catch(() => alert("Pokémon non trouvé !"));
 });
